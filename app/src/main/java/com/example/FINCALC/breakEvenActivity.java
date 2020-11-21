4ad4846afc9b7be7f0d7bad09c033fe09dc1a465
Anish Thakur <real.anish9@gmail.com>
2020-11-20 18:09:42 -0600

    completed breakEven Activity with some error handling

--- app/src/main/java/com/example/FINCALC/breakEvenActivity.java
+++ app/src/main/java/com/example/FINCALC/breakEvenActivity.java
@@ -1,14 +1,66 @@
 package com.example.FINCALC;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class breakEvenActivity extends AppCompatActivity {
-
+    private Button clear, calculate;
+    private TextView fixedVar,revenueVar,variableVar,brOutputVar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_break_even);
+
+        fixedVar=findViewById(R.id.fixedCostInput);
+        revenueVar=findViewById(R.id.revenueInput);
+        variableVar=findViewById(R.id.variableInput);
+        brOutputVar=findViewById(R.id.breakEvenOutput);
+        clear=findViewById(R.id.clear_buttonBr);
+        calculate=findViewById(R.id.calculateButtonBr);
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fixedVar.setText("");
+                revenueVar.setText("");
+                variableVar.setText("");
+                brOutputVar.setText("");
+            }
+        });
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float fixedCost,revenue, variableCost;
+                int breakEvenPoint=0;
+
+                fixedCost=Float.parseFloat(fixedVar.getText().toString());
+                revenue=Float.parseFloat(revenueVar.getText().toString());
+                variableCost=Float.parseFloat(variableVar.getText().toString());
+
+                breakEvenPoint= (int) (fixedCost/(revenue-variableCost));
+                if(breakEvenPoint<0)
+                {
+                    revenueVar.setError("should be higher than variable cost");
+                    AlertDialog.Builder builder= new AlertDialog.Builder(breakEvenActivity.this);
+                    builder.setMessage("Cost higher than Selling Price")
+                            .setNegativeButton("Cancel",null);
+
+                    AlertDialog alert =builder.create();
+                    alert.show();
+                }
+                else
+                {
+                    brOutputVar.setText(String.valueOf(breakEvenPoint));
+                }
+
+            }
+        });
     }
 }
