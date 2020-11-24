ae3a5ba3cd4c0e26dbe96cf7bf86c1cb0cd96e73
Anish Thakur <real.anish9@gmail.com>
2020-11-24 16:04:45 -0600

    git practice

--- app/src/main/java/com/example/FINCALC/APRActivity.java
+++ app/src/main/java/com/example/FINCALC/APRActivity.java
@@ -13,11 +13,11 @@ public class APRActivity extends AppCompatActivity {
         setContentView(R.layout.activity_a_p_r);
 
 
-        String [] loanType={"year","month"};
+        /*String [] loanType={"year","month"};
         Spinner loanSpinner= findViewById(R.id.termSpinner);
         ArrayAdapter<String> adapter;
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, loanType);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        loanSpinner.setAdapter(adapter);
+        loanSpinner.setAdapter(adapter);*/
     }
 }
