51e2a1809885f6f98487da241a68a162fb4eea81
unique-k <63078849+unique-k@users.noreply.github.com>
2020-11-25 14:26:50 -0600

    loan functions done, added keyboard end function.

--- app/src/main/java/com/example/FINCALC/MainActivity.java
+++ app/src/main/java/com/example/FINCALC/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button unitConversion;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         unitConversion=findViewById(R.id.buttonUC);
         loanCalculate=findViewById((R.id.loanButton));
